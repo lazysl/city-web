3338ef151f7733741c05914342eeae7c52194e58
lazysl <1101950957@qq.com>
2019-10-19 22:00:30 +0800

    城管

--- js/public.js
+++ js/public.js
@@ -5,3 +5,8 @@ $(".select ul li").click(function () {
     var name=$(this).children("span").text();
     $(this).parents("ul").siblings("span").empty().html(name+"<i></i>")
 });
+var docHeight=document.documentElement.clientHeight;
+var mainHeight=document.getElementById("main").clientHeight;
+if (mainHeight<docHeight){
+    $("#main").css("height",docHeight)
+}
