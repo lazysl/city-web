747ab2bdfcfdb8eefe03f7f228d5c9272228bdbf
lazysl <1101950957@qq.com>
2019-10-20 17:32:56 +0800

    城管

--- js/public.js
+++ js/public.js
@@ -5,8 +5,18 @@ $(".select ul li").click(function () {
     var name=$(this).children("span").text();
     $(this).parents("ul").siblings("span").empty().html(name+"<i></i>")
 });
+/*
 var docHeight=document.documentElement.clientHeight;
 var mainHeight=document.getElementById("main").clientHeight;
 if (mainHeight<docHeight){
     $("#main").css("height",docHeight)
-}
+}*/
+$("#icon_nav").click(function () {
+    $("#main").toggleClass("collapse");
+});
+$(function () {
+    var param = window.location.hash;
+    if (param.indexOf("collapse")>-1){
+        $("#main").addClass("collapse");
+    }
+})
