14cfe953ae3efcad4409525606e29786398a3c8f
lazysl <1101950957@qq.com>
2019-10-27 16:55:19 +0800

    城管

--- js/public.js
+++ js/public.js
@@ -19,4 +19,38 @@ $(function () {
     if (param.indexOf("collapse")>-1){
         $("#main").addClass("collapse");
     }
+});
+var setting={
+    www_url:"http://api.city.bli7.com",
+};
+$.extend({
+    jsonAjax: function (options, callbackSuc, callbackErr) {
+        options = $.extend(options, {"_r": Math.random()});
+        $.ajax({
+            type: options.ajaxtype,
+            url: options.url,
+            async: true,
+            data: options,
+            dataType: "json",
+            success: function (data) {
+                if ($.isFunction(callbackSuc)) callbackSuc(data);
+            },
+            error: function (data) {
+                if ($.isFunction(callbackErr)) callbackErr(data);
+            }
+        });
+    },
+    getAjax: function (param, callbackSuc, callbackErr) {
+        param = $.extend(param, {"ajaxtype": "GET"});
+        $.jsonAjax(param, callbackSuc, callbackErr);
+    },
+    postAjax: function (param, callbackSuc, callbackErr) {
+        param = $.extend(param, {"ajaxtype": "POST"});
+        $.jsonAjax(param, callbackSuc, callbackErr);
+    },
+    initNav: function () {
+        var param = {url: setting.www_url + "/public/sys/menu/nav?apiKey=" + setting.apiKey + "&name=" + name};
+        $.getAjax(param, function (data) {
+        })
+    }
 })
