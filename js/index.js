6bb80bc776a4e9dce6a92008fa52c9fa9af52adf
lazysl <1101950957@qq.com>
2020-01-23 13:33:11 +0800

    城管

--- js/index.js
+++ js/index.js
@@ -28,7 +28,8 @@ $.extend({
         $.getAjax(getWaring(), function (res) {
             if (res.code = 200 && res.code_desc == "success") {
                 var data = res.data;
-                var html = '', name,type, pic1 = "icon9", pic2 = "icon10", pic3 = "icon11",explain1="严重",explain2="一般",explain3="正常";
+                var html = '', name, type, pic1 = "icon9", pic2 = "icon10", pic3 = "icon11", explain1 = "严重",
+                    explain2 = "一般", explain3 = "正常";
                 if (data) {
                     for (var i in data) {
                         var totalRecords = data[i].totalRecords;
@@ -59,14 +60,14 @@ $.extend({
                         }
                         html += '<div class="layout">\n' +
                             '         <div class="item">\n' +
-                            '               <p onclick="$.getDetail('+type+')"><span>' + totalRecords + '</span>' + name + '</p>\n' +
+                            '               <p onclick="$.getDetail(' + type + ')"><span>' + totalRecords + '</span>' + name + '</p>\n' +
                             '               <img src="images/index/icon-' + i + '.png"/>\n' +
                             '         </div>\n' +
                             '         <div class="item-1">\n' +
                             '              <ul>\n' +
-                            '                  <li onclick="$.getDetail('+type+',1)"><img src="images/index/' + pic1 + '.png"/><p>' + critical + '</p><em>'+explain1+'</em></li>\n' +
-                            '                  <li onclick="$.getDetail('+type+',2)"><img src="images/index/' + pic2 + '.png"/><p>' + warning + '</p><em>'+explain2+'</em></li>\n' +
-                            '                  <li onclick="$.getDetail('+type+',3)"><img src="images/index/' + pic3 + '.png"/><p>' + clear + '</p><em>'+explain3+'</em></li>\n' +
+                            '                  <li onclick="$.getDetail(' + type + ',1)"><img src="images/index/' + pic1 + '.png"/><p>' + critical + '</p><em>' + explain1 + '</em></li>\n' +
+                            '                  <li onclick="$.getDetail(' + type + ',2)"><img src="images/index/' + pic2 + '.png"/><p>' + warning + '</p><em>' + explain2 + '</em></li>\n' +
+                            '                  <li onclick="$.getDetail(' + type + ',3)"><img src="images/index/' + pic3 + '.png"/><p>' + clear + '</p><em>' + explain3 + '</em></li>\n' +
                             '              </ul>\n' +
                             '         </div>\n' +
                             '     </div>';
@@ -80,7 +81,7 @@ $.extend({
         $.getAjax(getCheckModeAnaly(), function (res) {
             if (res.code = 200 && res.code_desc == "success") {
                 var data = res.data;
-                var html = '',rankHtml='';
+                var html = '', rankHtml = '';
                 if (data) {
                     for (var i in data) {
                         if (Object.keys(data[i].information).length < 4) {
@@ -100,11 +101,11 @@ $.extend({
                             '       <td><span class="' + middlewareStyle + '"></span>' + data[i].information.middleware + '</td>\n' +
                             '       <td><span class="' + internetStyle + '"></span>' + data[i].information.internet + '</td>\n' +
                             '     </tr>';
-                        var percent = ((data[i].information.server).indexOf('0/') > -1 || (data[i].information.sql).indexOf('0/') > -1 || (data[i].information.middleware).indexOf('0/') > -1 || (data[i].information.internet).indexOf('0/') > -1)?"20%":"0%"
+                        var percent = ((data[i].information.server).indexOf('0/') > -1 || (data[i].information.sql).indexOf('0/') > -1 || (data[i].information.middleware).indexOf('0/') > -1 || (data[i].information.internet).indexOf('0/') > -1) ? "20%" : "0%"
                         rankHtml += '<li>\n' +
                             '       <p>' + data[i].name + '</p>\n' +
-                            '       <span><em style="width: '+percent+'"></em></span>\n' +
-                            '       <p>'+percent+'</p>\n' +
+                            '       <span><em style="width: ' + percent + '"></em></span>\n' +
+                            '       <p>' + percent + '</p>\n' +
                             '      </li>'
                     }
                     $("#table").empty().append(html);
@@ -136,31 +137,91 @@ $.extend({
         })
     },
     getDetail(type, waringType) {
-        var html="",explain
-        if (typeof waringType == "undefined") waringType = "";
-        $.getAjax(js_checkSqlList(type, waringType), function (res) {
+        var html = "", explain, color, waringTypes;
+        if (typeof waringType == "undefined") waringTypes = "";
+        else {
+            if (type == 5) {
+                if (waringType == 1) waringTypes = 1;
+                if (waringType == 2) waringTypes = 3;
+                if (waringType == 3) waringTypes = 2
+            } else {
+                waringTypes = waringType
+            }
+        }
+        $.getAjax(js_checkSqlList(type, waringTypes), function (res) {
             if (res.code = 200 && res.code_desc == "success") {
                 $(".tcHide").show();
                 var data = res.data;
                 if (data) {
-                    if (data.length>0){
+                    if (data.length > 0) {
                         for (var i in data) {
                             if (type == 5) {
-                                if (data[i].STATUS == "Resolved") explain = "运行";
-                                if (data[i].STATUS == "Open") explain = "已解决";
-                                if (data[i].STATUS == "closed") explain = "已关闭";
-                                html += '<li><span>' + data[i].SUBJECT + '</span><span>' + explain + '</span></li>'
+                                if (data[i].STATUS == "Resolved") {
+                                    explain = "已解决";
+                                }
+                                if (data[i].STATUS == "Open") {
+                                    explain = "运行";
+                                }
+                                if (data[i].STATUS == "closed") {
+                                    explain = "已关闭";
+                                }
+                                html += '<li><span>' + data[i].SUBJECT + '</span><span>' + data[i].CREATEDBY + '</span><span>' + explain + '</span></li>'
                             } else {
-                                if (data[i].status == "critical") explain = "严重";
-                                if (data[i].status == "warning") explain = "一般";
-                                if (data[i].status == "Clear"||data[i].status == "clear") explain = "正常";
-                                html += '<li><span>' + data[i].name + '</span><span>' + explain + '</span></li>'
+                                if (data[i].status == "critical") {
+                                    explain = "严重";
+                                    color = "critical"
+                                }
+                                if (data[i].status == "warning") {
+                                    explain = "一般";
+                                    color = "warning"
+                                }
+                                if (data[i].status == "Clear" || data[i].status == "clear") {
+                                    explain = "正常";
+                                    color = "clear"
+                                }
+                                html += '<li><span>' + data[i].name + '</span><span>' + data[i].displayName + '</span><span class="' + color + '">' + explain + '</span></li>'
                             }
                         }
-                    }else {
-                        html='<li><span>暂无数据</span></li>'
+                    } else {
+                        html = '<li>暂无数据</li>'
                     }
                     $(".tcHide ul").empty().append(html)
+                    if (type == 1) {
+                        if (waringTypes==''){
+                            $(".tcHide .tcTit span").empty().append("数据库概览")
+                        }else {
+                            if (waringTypes==1) $(".tcHide .tcTit span").empty().append("数据库-严重告警");
+                            if (waringTypes==2) $(".tcHide .tcTit span").empty().append("数据库-一般告警");
+                            if (waringTypes==3) $(".tcHide .tcTit span").empty().append("数据库-正常告警")
+                        }
+                    }
+                    if (type == 2) {
+                        if (waringTypes==''){
+                            $(".tcHide .tcTit span").empty().append("中间件概览")
+                        }else {
+                            if (waringTypes==1) $(".tcHide .tcTit span").empty().append("中间件-严重告警");
+                            if (waringTypes==2) $(".tcHide .tcTit span").empty().append("中间件-一般告警");
+                            if (waringTypes==3) $(".tcHide .tcTit span").empty().append("中间件-正常告警")
+                        }
+                    }
+                    if (type == 3) {
+                        if (waringTypes==''){
+                            $(".tcHide .tcTit span").empty().append("服务器概览")
+                        }else {
+                            if (waringTypes==1) $(".tcHide .tcTit span").empty().append("服务器-严重告警");
+                            if (waringTypes==2) $(".tcHide .tcTit span").empty().append("服务器-一般告警");
+                            if (waringTypes==3) $(".tcHide .tcTit span").empty().append("服务器-正常告警")
+                        }
+                    }
+                    if (type == 5) {
+                        if (waringTypes==''){
+                            $(".tcHide .tcTit span").empty().append("工单概览")
+                        }else {
+                            if (waringTypes==1) $(".tcHide .tcTit span").empty().append("工单-正在运行");
+                            if (waringTypes==2) $(".tcHide .tcTit span").empty().append("工单-已解决");
+                            if (waringTypes==3) $(".tcHide .tcTit span").empty().append("工单-已关闭")
+                        }
+                    }
                 }
             }
         })
