41f2ccbe666b41d2e2c590284b32c49a35d4b9a6
lazysl <1101950957@qq.com>
2020-03-15 23:10:57 +0800

    城管

--- js/infoSecurity.js
+++ js/infoSecurity.js
@@ -5,8 +5,8 @@ new Vue({
         infoSecurityList: [],
         deviceTxt: [
             {name: "服务器", id: 1, maxHeight: "", midHeight: "", minHeight: "",},
-            {name: "数据库", id: 1, maxHeight: "", midHeight: "", minHeight: "",},
-            {name: "中间件", id: 1, maxHeight: "", midHeight: "", minHeight: "",},
+            {name: "数据库", id: 2, maxHeight: "", midHeight: "", minHeight: "",},
+            {name: "中间件", id: 3, maxHeight: "", midHeight: "", minHeight: "",},
         ],
         leakPercent: "",   //安全漏洞比率
         virusPercent: "",   //病毒数比率
@@ -94,25 +94,24 @@ new Vue({
                     minHeight: this.deviceTxt[i].minHeight,
                 })
             }
+            let informationSecurity = [{
+                    leakPercent: this.leakPercent,   //安全漏洞比率
+                    virusPercent: this.virusPercent,   //病毒数比率
+                    virusNum: this.virusNum,   //病毒数
+                    portNum: this.portNum,   //端口数
+                    strongNum: this.strongNum,   //强力攻击数
+                    trojanNum: this.trojanNum,   //木马攻击数
+                    refuseNum: this.refuseNum,   //拒绝访问攻击数
+                    bufferNum: this.bufferNum,   //缓冲区溢出攻击数
+                    wormNum: this.wormNum,   //网络蠕虫攻击数
+                    ipNum: this.ipNum,   //IP碎片攻击数
+                    deviceTxt: deviceData,
+                }];
             let data = {
                 id: this.urlParams("id"),
-                informationSecurity: [
-                    {
-                        leakPercent: this.leakPercent,   //安全漏洞比率
-                        virusPercent: this.virusPercent,   //病毒数比率
-                        virusNum: this.virusNum,   //病毒数
-                        portNum: this.portNum,   //端口数
-                        strongNum: this.strongNum,   //强力攻击数
-                        trojanNum: this.trojanNum,   //木马攻击数
-                        refuseNum: this.refuseNum,   //拒绝访问攻击数
-                        bufferNum: this.bufferNum,   //缓冲区溢出攻击数
-                        wormNum: this.wormNum,   //网络蠕虫攻击数
-                        ipNum: this.ipNum,   //IP碎片攻击数
-                        deviceTxt: deviceData,
-                    }
-                ]
+                informationSecurity: JSON.stringify(informationSecurity)
             };
-            this.postAjax(this.updateCheckInfoSecurity(JSON.stringify(data)), (res) => {
+            this.postAjax(this.updateCheckInfoSecurity(data), (res) => {
                 if (res.code = 200 && res.code_desc == "success") {
                     this.initInfo();
                 } else alert(res.code_desc)
