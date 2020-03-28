fdba6ba73e78f6616e6054a01d57e836bfdbec67
lazysl <1101950957@qq.com>
2020-03-28 21:57:50 +0800

    城管

--- js/evaluationModel.js
+++ js/evaluationModel.js
@@ -2,42 +2,47 @@ new Vue({
     el: '#main',
     data: {
         objectTotal: 100,
+        technologyTotal: 60,
         technology: [
             {name: "服务器", id: 0, maxHeight: "", minHeight: "",},
             {name: "数据库", id: 1, maxHeight: "", minHeight: "",},
             {name: "中间件", id: 2, maxHeight: "", minHeight: "",},
         ],
         business: {
+            businessTotal: 40,
             health: [
-                {id: 0, qualification: "正常"},
-                {id: 1, qualification: "正常"},
-                {id: 2, qualification: ""},
-                {id: 3, qualification: "正常"},
+                {id: 0, qualification: "正常", fraction: 2},
+                {id: 1, qualification: "正常", fraction: 2},
+                {id: 2, qualification: "", fraction: 4},
+                {id: 3, qualification: "正常", fraction: 2},
             ],
+            healthTotal: 10,
             security: [
-                {id: 0, percent: "", maxHeight: "", midHeight: "", minHeight: ""},
-                {id: 1, percent: "", num: ""},
-                {id: 2, num: ""},
-                {id: 3, num: ""},
-                {id: 4, num: ""},
-                {id: 5, num: ""},
-                {id: 6, num: ""},
-                {id: 7, num: ""},
-                {id: 8, num: ""},
+                {id: 0, percent: "", maxHeight: "", midHeight: "", minHeight: "", fraction: 1},
+                {id: 1, percent: "", num: "", fraction: 1},
+                {id: 2, num: "", fraction: 3},
+                {id: 3, num: "", fraction: 2},
+                {id: 4, num: "", fraction: 1},
+                {id: 5, num: "", fraction: 1},
+                {id: 6, num: "", fraction: 3},
+                {id: 7, num: "", fraction: 2},
+                {id: 8, num: "", fraction: 1},
             ],
+            securityTotal: 15,
             internet: [
-                {id: 0, num: ""},
-                {id: 1, num: ""},
-                {id: 2, num: ""},
-                {id: 3, num: ""},
-                {id: 4, num: ""},
-                {id: 5, num: ""},
-                {id: 6, num: ""},
-                {id: 7, num: ""},
-                {id: 8, num: ""},
-                {id: 9, num: ""},
-                {id: 10, num: ""},
+                {id: 0, num: "", fraction: 1},
+                {id: 1, num: "", fraction: 2},
+                {id: 2, num: "", fraction: 2},
+                {id: 3, num: "", fraction: 1},
+                {id: 4, num: "", fraction: 1},
+                {id: 5, num: "", fraction: 3},
+                {id: 6, num: "", fraction: 1},
+                {id: 7, num: "", fraction: 1},
+                {id: 8, num: "", fraction: 1},
+                {id: 9, num: "", fraction: 1},
+                {id: 10, num: "", fraction: 1},
             ],
+            internetTotal: 15,
         },
         activeSubIndex: -1,
         activeThreeIndex: -1,
@@ -93,8 +98,16 @@ new Vue({
                         let rule = "";
                         if (res.data[i].id == 1) {
                             rule = JSON.parse(res.data[i].rule);
+                            console.log(rule)
+                            this.objectTotal = rule.objectTotal;
+                            this.technologyTotal = rule.technologyTotal;
+                            this.business.businessTotal = rule.business.businessTotal;
+                            this.business.healthTotal = rule.business.healthTotal;
+                            this.business.securityTotal = rule.business.securityTotal;
+                            this.business.internetTotal = rule.business.internetTotal;
                             for (let i in rule.business.health) {
                                 this.business.health[i].qualification = rule.business.health[i].qualification;
+                                this.business.health[i].fraction = rule.business.health[i].fraction;
                             }
                             for (let j in rule.business.security) {
                                 if (j == 0) {
@@ -108,9 +121,11 @@ new Vue({
                                 } else {
                                     this.business.security[j].num = rule.business.security[j].num;
                                 }
+                                this.business.security[j].fraction = rule.business.security[j].fraction;
                             }
                             for (let h in rule.business.internet) {
                                 this.business.internet[h].num = rule.business.internet[h].num;
+                                this.business.internet[h].fraction = rule.business.internet[h].fraction;
                             }
                             for (let k in rule.technology) {
                                 this.technology[k].maxHeight = rule.technology[k].maxHeight;
@@ -118,7 +133,7 @@ new Vue({
                             }
                         }
                     }
-                }else alert(res.code_desc)
+                } else alert(res.code_desc)
             })
         },
         saveModel() {
@@ -128,6 +143,7 @@ new Vue({
                 healthData.push({
                     id: i,
                     qualification: this.business.health[i].qualification,
+                    fraction: this.business.health[i].fraction,
                 })
             }
             for (let j in this.business.security) {
@@ -138,17 +154,20 @@ new Vue({
                         maxHeight: this.business.security[j].maxHeight,
                         midHeight: this.business.security[j].midHeight,
                         minHeight: this.business.security[j].minHeight,
+                        fraction: this.business.security[j].fraction,
                     })
                 } else if (j == 1) {
                     securityData.push({
                         id: j,
                         percent: this.business.security[j].percent,
                         num: this.business.security[j].num,
+                        fraction: this.business.security[j].fraction,
                     })
                 } else {
                     securityData.push({
                         id: j,
                         num: this.business.security[j].num,
+                        fraction: this.business.security[j].fraction,
                     })
                 }
 
@@ -157,6 +176,7 @@ new Vue({
                 internetData.push({
                     id: h,
                     num: this.business.internet[h].num,
+                    fraction: this.business.internet[h].fraction,
                 })
             }
             for (let k in this.technology) {
@@ -164,15 +184,22 @@ new Vue({
                     id: k,
                     maxHeight: this.technology[k].maxHeight,
                     minHeight: this.technology[k].minHeight,
+                    fraction: this.technology[k].fraction,
                 })
             }
             rule = {
                 business: {
+                    businessTotal: this.business.businessTotal,
                     health: healthData,
+                    healthTotal: this.business.healthTotal,
                     security: securityData,
-                    internet: internetData
+                    securityTotal: this.business.securityTotal,
+                    internet: internetData,
+                    internetTotal: this.business.internetTotal,
                 },
                 technology: technologyData,
+                technologyTotal: this.technologyTotal,
+                objectTotal: this.objectTotal
             };
             data = {
                 id: 1,
